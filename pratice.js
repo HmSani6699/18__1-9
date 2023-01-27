cd081e2e80c635364b84c66b153fed9d5b9ae6d9
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 23:05:10 +0600

    ad to nastad condition in number

--- pratice.js
+++ pratice.js
@@ -10,10 +10,11 @@ fruits[1] = 'Mango';
 
 // Practice Problem 2
 
+var totalMark = 85;
+if (totalMark)
 
-
-// Practice Problem 3
-var number1 = 13;
+    // Practice Problem 3
+    var number1 = 13;
 var number2 = 69;
 var number3 = 45;
 
@@ -21,8 +22,37 @@ if (number1 >= number2 && number1 >= number3) {
     console.log(number1)
 }
 else if (number2 >= number1 && number2 >= number3) {
-    console.log(number2)
+    // console.log(number2)
 }
 else {
-    console.log(number3)
+    // console.log(number3)
+}
+
+var num1 = 3;
+var num2 = 2;
+var num3 = 3;
+if (num1 == num2 || num1 == num3) {
+    // console.log('your grate')
+}
+else {
+    console.log('tomader mell sarajino hobe na')
+}
+
+
+
+var roll_1 = 20;
+var roll_2 = 40;
+var roll_3 = 10;
+var roll_4 = 60;
+if (roll_1 >= roll_2 && roll_1 >= roll_3 && roll_1 >= roll_4) {
+    console.log(roll_1);
+}
+else if (roll_2 >= roll_1 && roll_2 >= roll_3 && roll_2 >= roll_4) {
+    console.log(roll_2)
+}
+else if (roll_3 >= roll_1 && roll_3 >= roll_2 && roll_3 >= roll_4) {
+    console.log(roll_3)
+}
+else {
+    console.log(roll_4)
 }
