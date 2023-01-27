342ecca5c1fa9874d4e10f02b39ae4c1c848ff34
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 18:38:59 +0600

    chack your variavle to index number

--- comparison.js
+++ comparison.js
@@ -1,7 +1,7 @@
 // console.log(5 < 2);
 // console.log(5 > 2);
 // console.log(5 <= 2);
-console.log(5 >= 2);
+// console.log(5 >= 2);
 // console.log(5 === 2);
 // console.log(5 !== 2);
 
@@ -16,9 +16,9 @@ let myArray = [1, 2, 3, 4, 5];
 let x = 5;
 let y = 10;
 if (x < y) {
-    console.log("x is less than y");
+    // console.log("x is less than y");
 } else {
     console.log("x is greater than or equal to y");
 }
 // console.log(6 != 6);
-console.log(6 >= 6)
+// console.log(6 >= 6)
--- index.js
+++ index.js
@@ -11,4 +11,17 @@ var element = num[0];
 
 //element diye index number
 var thart = num.indexOf(10);
-console.log(thart);
+// console.log(thart);
+
+
+//declare your array
+var number = [10, 30, 49, 95, 30, 60];
+console.log(number);
+//chance your element
+number[1] = 40;
+console.log(number);
+//chack yuor index number in element
+// console.log(number[1]);
+
+//chack your element in index number
+// console.log(number.indexOf(30));
