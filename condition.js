d46fc5bf5a7c5673b1e2ac243123628d6622fec6
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 21:06:16 +0600

    ad  to conditaion if else part

---
+++ condition.js
@@ -0,0 +1,35 @@
+var myMoney = 500;
+var yourMoney = 1000;
+if (myMoney > yourMoney) {
+    console.log('ami choto manos amake tomra valo baso na')
+}
+else {
+    // console.log("tomi ki vallo")
+}
+
+
+var alo = 25;
+var tomato = 30;
+var lobon = 40;
+var ada = 50;
+var roson = 60;
+var maney = 35;
+
+if (alo > maney) {
+    console.log('ami alo khate vallo moja pai')
+}
+else if (tomato > maney) {
+    console.log('tomato apple er posti dey')
+}
+else if (lobon > maney) {
+    console.log('lobon diye bat kai')
+}
+else if (ada > maney) {
+    console.log('ada mama kinto  khive jal')
+}
+else if (roson > maney) {
+    console.log('roson vallo baso')
+}
+else {
+    console.log('ami tomader kaoke cai na')
+}
