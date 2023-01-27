8133fc08e143e9b64dabd550054f5aac8ed3be5a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 12:39:55 +0600

    ad to index number

---
+++ index.js
@@ -0,0 +1,14 @@
+var num = [12, 23, 34, 45, 56, 67, 78, 10];
+
+//index number in set by balu
+num[0] = 24;
+// console.log(num);
+
+
+//index number diya element
+var element = num[0];
+// console.log(element);
+
+//element diye index number
+var thart = num.indexOf(10);
+console.log(thart);
