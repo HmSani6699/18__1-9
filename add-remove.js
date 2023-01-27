e36187e44f0f3b74b9eb41ba95b2d316ecbbb63b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 14:47:46 +0600

    ad to array in first element and last element

---
+++ add-remove.js
@@ -0,0 +1,20 @@
+var friendsName = ["sadiq", "arman", "labib", "abol"];
+// console.log(friendsName);
+//add or array in last element
+friendsName.push("babu");
+
+//remove in array to last element
+friendsName.pop();
+friendsName.pop();
+friendsName.pop();
+friendsName.pop();
+
+//add to array in first element
+friendsName.unshift("raja", "mahmod");
+
+
+//remove in array to first element
+friendsName.shift()
+friendsName.shift()
+console.log(friendsName);
+
