ff60b098b8bf43a8008dd87bb8ae34cab288f8ca
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-27 18:23:13 +0600

    try to variavle counding

--- array.js
+++ array.js
@@ -2,5 +2,11 @@ var friendsName = ["sani", "babu", "raju", "pappo"];
 
 var friendsAge = [10, 30, 19, 40, 20,];
 
-console.log(friendsAge.length);
+// console.log(friendsAge.length);
+
+
+
+var studentNames = ['Sumit', 'jhankar', 'anis', 'ali'];
+var allName = studentNames.length;
+// console.log(allName);
 
---
+++ comparison.js
@@ -0,0 +1,24 @@
+// console.log(5 < 2);
+// console.log(5 > 2);
+// console.log(5 <= 2);
+console.log(5 >= 2);
+// console.log(5 === 2);
+// console.log(5 !== 2);
+
+var arr = [3, 7, 10];
+var second = arr.indexOf(7);
+// console.log(second)
+// console.log(arr[1])
+
+let myArray = [1, 2, 3, 4, 5];
+// console.log(myArray[2]);
+
+let x = 5;
+let y = 10;
+if (x < y) {
+    console.log("x is less than y");
+} else {
+    console.log("x is greater than or equal to y");
+}
+// console.log(6 != 6);
+console.log(6 >= 6)
